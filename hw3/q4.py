0acf27edb6370d064e35eb6e4892a24d2bb01c83
Andrew Yip <andrew.yip22@gmail.com>
2021-02-08 10:35:09 -1000

    added time module to calculate time to hw3 q4

--- hw3/q4.py
+++ hw3/q4.py
@@ -25,8 +25,43 @@ def bubble_sort(arr):
 
     return arr
 
-arr = [5,4,3,2,1]
+list1=[random.randint(0,10) for i in range(100)]
+print(list1)
+st1 = time.time()
+print(bubble_sort(list1))
+arr1time = time.time()-st1
+print(arr1time)
+print("\n")
+
+list2=[random.randint(0,10) for i in range(100)]
+print(list2)
+st2 = time.time()
+print(bubble_sort(list2))
+arr2time = time.time()-st2
+print(arr2time)
+print("\n")
+
+list3=[random.randint(0,10) for i in range(100)]
+print(list3)
+st3 = time.time()
+print(bubble_sort(list3))
+arr3time = time.time()-st3
+print(arr3time)
+print("\n")
+
 list4=[random.randint(0,10) for i in range(100)]
-print(arr)
-print(bubble_sort(arr))
+print(list4)
+st4 = time.time()
+print(bubble_sort(list4))
+arr4time = time.time()-st4
+print(arr4time)
+print("\n")
+
+list4=[random.randint(0,10) for i in range(100)]
+print(list4)
+st4 = time.time()
+print(bubble_sort(list4))
+arr4time = time.time()-st4
+print(arr4time)
+print("\n")
 
