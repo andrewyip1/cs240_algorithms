9b6e0cf0e0b9d69958cb7b27670667ebde32685e
Andrew Yip <andrew.yip22@gmail.com>
2021-02-18 14:24:31 -1000

    modified karatsuba algorithm

--- hw6-7/karatsuba_algorithm.py
+++ hw6-7/karatsuba_algorithm.py
@@ -5,8 +5,12 @@ import math
 
 def karatsuba(x,y):
 
+    #to count the number of times function gets called
+    global count
+    count += 1
+
     # Base Case:
-    if x < 10 and y < 10:
+    if x < 10 or y < 10:
         return x*y
 
     n = max(int(math.log10(x)+1),int(math.log10(y+0.001)+1))
@@ -27,6 +31,8 @@ def karatsuba(x,y):
     #Main Equation - x*y = ((10^n) * ac) + ((10^n/2) * (ad + bd)) + bd
     return int((10**(half_n*2)*ac) + (10**half_n)*S4 + bd)
 
-print(karatsuba(1234567891011121314151617181920212223242526272829303132333435363,1357911131517192123252729313335373941434547495153555759616365676))
+count = 0
+print(karatsuba(1234,1212))
+print('number of times karatsuba gets called: ', count)
 
 
